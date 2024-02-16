c25299e296b3b324ea276aaa78b9ff4afb8ca8f0
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-16 17:47:47 -0500

    Create Lab2-Part2.c

---
+++ Lab2-Part2.c
@@ -0,0 +1,14 @@
+// This program is designed to compute the binary number of any decimal from 0-32 
+// The following code is written and compiled with CLion
+//
+// Program: Lab2-Part2.c
+// Purpose: To compute the binary number of any decimal from 0-32 
+
+// Author: Julia Piascik
+// Course ENGR200 Computational Method in Engineering
+// Term Spring 2024
+// Date: 2/19/2024
+
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
