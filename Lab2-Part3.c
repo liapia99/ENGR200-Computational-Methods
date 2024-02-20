e26fd749f65e11e7d4857ad90aa6e4c70539be90
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-19 23:22:52 -0500

    Create Lab2-Part3.c

---
+++ Lab2-Part3.c
@@ -0,0 +1,39 @@
+// This program is designed to make the given code error free.
+// The following code is written and compiled with VSCode
+//
+// Program: Lab2-Part3.c
+// Purpose: To make the given code error free.
+//
+// Author: Julia Piascik
+// Course ENGR200 Computational Method in Engineering
+// Term Spring 2024
+// Date: 2/22/2024
+
+
+#define CUBE(x) ((x)*(x)*(x));
+#define SQ(x) ((x)*(x));
+
+int main(){
+    double xb,x_y,y3z,z;
+    int Float, character, x, a, b, c, d, pa_b, qa_b, x1;
+
+    char integer,u, _2v,w ='t';
+    a = 2, b = 3;
+    c = a + b;
+    //b - a == c;
+    d = w;
+    a = CUBE(d);
+    b = SQ(d);
+    u = d + 62;
+    c = u - 1;
+    u='y';
+    _2v = z;
+    y3z = CUBE(c);
+    x = SQ(c);
+    x1 = x * 2;
+    c = x + u;
+    return 0;
+}
+
+
+
