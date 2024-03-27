6942ab15b8792850d02c28a0138cf847001eedab
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-03-27 11:49:56 -0400

    Update exercise2.m

--- MATLAB/Lab 1/exercise2.m
+++ MATLAB/Lab 1/exercise2.m
@@ -1,13 +1,41 @@
+%using plot command for multiple plots 
 x = 0:pi/30:2*pi;
 y = sin(x);
 z = cos(x);
 
-plot(y,z, 'm+:');
+plot(x, y, '--r')
+hold on 
+plot(x, z, 'b*')
+xlabel('x')
+ylabel('y')
+title('Plot of sin(x) and cos(x)')
+legend('sin(x)', 'cos(x)')
 
-plot(z);
-plot(y);
 
-c = 3*cos(n*x);
-x = 0:pi:2*pi;
-ezplot(c)
+%using plot command for single plot
+x = 0:pi/30:2*pi;
+y = sin(x);
+z = cos(x);
 
+
+data = [y; z];
+
+
+plot(x, data, '--')
+xlabel('x')
+ylabel('y')
+title('Plot of sin(x) and cos(x)')
+legend('sin(x)', 'cos(x)')
+hold on 
+
+%using ezplot command to plot
+syms x; % define x as a symbolic variable
+n = 1;
+y = 3 * cos(n * x);
+
+ezplot(y, [0, 2*pi]) 
+xlabel('x')
+ylabel('y')
+title('Plot Part 3 of Ex.3')
+legend('f(x)')
+hold on
